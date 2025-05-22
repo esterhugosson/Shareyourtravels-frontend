4e207d5ca8b51a640f9fccc039c86cf431af5e92
Ester Hugosson <eh225bx@student.lnu.se>
2025-05-22 14:28:28 +0200

    Your account component

--- src/components/Youraccount/Youraccount.jsx
+++ src/components/Youraccount/Youraccount.jsx
@@ -1,9 +1,12 @@
 import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
 import './Youraccount.css'
+import { useState } from 'react'
+import { AccountForm } from './updateAccount'
 
 export const Youraccount = () => {
 
     const authUser = useAuthUser()
+    const [showForm, setShowForm] = useState(false)
 
     // Defensive check in case authUser is null (e.g., before login or on logout)
     if (!authUser) {
@@ -13,27 +16,25 @@ export const Youraccount = () => {
     const { firstName, lastName, email, username } = authUser;
 
     return (
-        <div className='container'>
-
-
+         <div className='container'>
             <h2>Your Account</h2>
 
-
-            <ul>
-                <li><strong>First Name:</strong> {firstName}</li>
-                <li><strong>Last Name: </strong>{lastName}</li>
-                <li><strong>Username: </strong>{username}</li>
-                <li><strong>Email:</strong> {email}</li>
-        </ul>
-       
-
-
-        </div >
+            {!showForm ? (
+                <>
+                    <ul>
+                        <li><strong>First Name:</strong> {firstName}</li>
+                        <li><strong>Last Name: </strong>{lastName}</li>
+                        <li><strong>Username: </strong>{username}</li>
+                        <li><strong>Email:</strong> {email}</li>
+                    </ul>
+                    <button onClick={() => setShowForm(true)}>Update</button>
+                    <button>Delete</button>
+                </>
+            ) : (
+                <AccountForm onCancel={() => setShowForm(false)} />
+            )}
+        </div>
      )
-
-
-
-
 }
 
 export default Youraccount
